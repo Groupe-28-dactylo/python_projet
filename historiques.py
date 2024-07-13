5feb0fcb2241ff3e975bcfc28c06f73b9df02fef
chris-mayamba <christianmayamba04@gmail.com>
2024-07-13 13:16:32 +0200

    implémentation du fichier historiques.py

---
+++ historiques.py
@@ -0,0 +1,25 @@
+import json
+
+def ajouter_historique(action, article, quantite):
+    historique = charger_historique()
+    modification = {
+        "Action": action,
+        "Article": article,
+        "Quantite": quantite,
+        "Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    historique.append(modification)
+    enregistrer_historique(historique)
+
+def enregistrer_historique(historique):
+    with open('historique.json', 'w') as f:
+        json.dump(historique, f, indent=4)
+    print("Historique mis à jour dans historique.json")
+
+def charger_historique():
+    try:
+        with open('historique.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("Le fichier historique.json n'existe pas.")
+        return []
